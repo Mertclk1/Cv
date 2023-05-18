2267bc623c78b0255567db10af38e11098b71282
Hasan Mert Çelik <hmert.clk@gmail.com>
2023-05-18 18:32:45 +0300

    NavBar Düzenlemesi

---
+++ Cv/ViewComponents/Dashboard/AdminNavbarMessageList.cs
@@ -0,0 +1,17 @@
+﻿using Cv.Business.Concrete;
+using Cv.DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cv.UI.ViewComponents.Dashboard
+{
+	public class AdminNavbarMessageList : ViewComponent
+	{
+		WriterMessageManager _writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+		public IViewComponentResult Invoke()
+		{
+			string p = "admin@gmail.com";
+			var values = _writerMessageManager.GetListReceiverMessage(p).OrderByDescending(x=>x.WtiterMessageId).Take(3).ToList();
+			return View(values);
+		}
+	}
+}
