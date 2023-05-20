0ce02abdd2853921e4d5f4a8a91891390d624c0e
Hasan Mert Çelik <hmert.clk@gmail.com>
2023-05-20 12:54:28 +0300

    düzenleme

---
+++ Cv/Controllers/AdminLoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cv.UI.Controllers
+{
+	public class AdminLoginController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
