fc28bf12c13b79da6c8e1073fba1a18d58096e1a
Hasan Mert Çelik <hmert.clk@gmail.com>
2023-05-20 19:16:35 +0300

    Ajax başlangıcı yapıldı Writer katmanları çıkıldı

---
+++ Cv/Controllers/WriterUserController.cs
@@ -0,0 +1,26 @@
+﻿using Cv.Business.Concrete;
+using Cv.DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Cv.UI.Controllers
+{
+    public class WriterUserController : Controller
+    {
+        WriterManager WriterManager = new WriterManager(new EfWriterDal());
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public IActionResult UserList() 
+        {
+            var values = JsonConvert.SerializeObject(WriterManager.TGetList());
+
+            return Json (values);
+
+        }
+        
+    }
+}
