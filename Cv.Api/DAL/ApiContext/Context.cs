518e7ea2a71417cdd0d55e500243ab62e48dc12a
Hasan Mert Çelik <hmert.clk@gmail.com>
2023-05-21 14:51:26 +0300

    Api Katmanı etributeleri eklendi

---
+++ Cv.Api/DAL/ApiContext/Context.cs
@@ -0,0 +1,16 @@
+﻿using Cv.Api.DAL.Entity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cv.Api.DAL.ApiContext
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MERT; Database=Cv2; User Id=sa; Password =123");
+        }
+
+        public DbSet<Category> categories { get; set; }
+    }
+}
---
+++ Cv.Api/Migrations/20230521112400_1001_FirstMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cv.Api.Migrations
+{
+    public partial class _1001_FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categories", x => x.CategoryID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "categories");
+        }
+    }
+}
