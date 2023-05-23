6e526cd8af372d4c875d342d2aab84ab77e90995
Hasan Mert Çelik <hmert.clk@gmail.com>
2023-05-23 13:39:33 +0300

    Düzenleme

--- Cv/Areas/Writer/Controllers/DefaultController.cs
+++ Cv/Areas/Writer/Controllers/DefaultController.cs
@@ -4,27 +4,29 @@ using Cv.DataAccess.EntityFramework;
 using Cv.Entity.Classes;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace Cv.UI.Areas.Writer.Controllers
+namespace Core_Proje.Areas.Writer.Controllers
 {
-	[Area("Writer")]
-	[Authorize]
-    [Route("Writer/[controller]/[action]")]
+    [Area("Writer")]
+    [Authorize]
     public class DefaultController : Controller
-	{
-		AnnouncementManager announcementManager = new AnnouncementManager(new EfAnnouncementDal());
-		public IActionResult Index()
-		{
-			var values = announcementManager.TGetList();
-			return View(values);
-		}
+    {
+        AnnouncementManager announcementManager = new AnnouncementManager(new EfAnnouncementDal());
+        public IActionResult Index()
+        {
+            var values = announcementManager.TGetList();
+            return View(values);
+        }
 
-		[HttpGet]
-		public IActionResult AnnouncementDetails(int id)
-		{
-			var values = announcementManager.TGetByID(id);
-			return View(values);
-		}
-
-	}
+        [HttpGet]
+        public IActionResult AnnouncementDetails(int id)
+        {
+            Announcement announcement = announcementManager.TGetByID(id);
+            return View(announcement);
+        }
+    }
 }
